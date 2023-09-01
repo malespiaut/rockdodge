4dd792baa1c50f38aa232f26373556888e5c17ab
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-09-01 20:00:01 +0200

    Adding Zig SDL2 boilerplate.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,39 @@
 const std = @import("std");
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
+const k_screen_width: i32 = 320;
+const k_screen_height: i32 = 320;
+
+var g_window: ?*c.SDL_Window = null;
+var g_renderer: ?*c.SDL_Renderer = null;
+var g_texture: ?*c.SDL_Texture = null;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
+        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitFailed;
+    }
+    defer c.SDL_Quit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    g_window = c.SDL_CreateWindow("", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, k_screen_width, k_screen_height, c.SDL_WINDOW_SHOWN | c.SDL_WINDOW_ALLOW_HIGHDPI) orelse
+        {
+        c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
+        return error.SDLCreateWindowFailed;
+    };
+    defer c.SDL_DestroyWindow(g_window);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    g_renderer = c.SDL_CreateRenderer(g_window, -1, c.SDL_RENDERER_PRESENTVSYNC) orelse
+        {
+        c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
+        return error.SDLCreateRendererFailed;
+    };
+    defer c.SDL_DestroyRenderer(g_renderer);
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    if (c.SDL_RenderSetLogicalSize(g_renderer, k_screen_width, k_screen_height) != 0) {
+        c.SDL_Log("Unable to set independent resolution for rendering: %s", c.SDL_GetError());
+        //XXX: Not sure if execution should stop because of that.
+        //return error.SDLRenderSetLogicalSizeFailed;
+    }
 }
